d13ed44f937c43bbac48bde9ff7bdd810fb4ae81
Mike Litzow <mlitzow@alaska.edu>
2019-11-16 15:27:14 -0900

    Cleaned up plots

--- PDO and climate.R
+++ PDO and climate.R
@@ -17,18 +17,20 @@ dat <- read.csv("data/climate.data.csv", row.names = 1)
 cb <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 # now plot relative to PDO!
-colnames(dat)[c(2,3,5,6)] <- c("GOA Sea surface height (Feb-Apr)",
-                               "GOA Wind stress (Feb-Apr)",
-                               "GOA Sea surface temp. (Nov-Mar)", 
-                             "North Pacific Index (Nov-Mar)")
+colnames(dat)[c(2,3,5,6)] <- c("Sea surface height (m, Feb-Apr)",
+                               "Wind stress (pa, Feb-Apr)",
+                               "Sea surface temp. (ºC, Nov-Mar)", 
+                             "North Pacific Index (mb, Nov-Mar)")
 dat <- dat %>%
   gather(key, value, -NDJFM.PDO, -era)
 
 dat$plot.era <- ifelse(dat$era==1, "1964-1988", 
                        ifelse(dat$era==2, "1989-2013", "2014-2019"))
-dat$key.order <- ifelse(dat$key=="North Pacific Index (Nov-Mar)", 1, 
-                        ifelse(dat$key=="GOA Sea surface temp. (Nov-Mar)", 2,
-                        ifelse(dat$key=="GOA Sea surface height (Feb-Apr)", 3, 4)))
+
+dat$key.order <- ifelse(dat$key=="North Pacific Index (mb, Nov-Mar)", 1, 
+                        ifelse(dat$key=="Sea surface temp. (ºC, Nov-Mar)", 2,
+                        ifelse(dat$key=="Sea surface height (m, Feb-Apr)", 3, 4)))
+
 dat$key <- reorder(dat$key, dat$key.order)
 
 scatter <- ggplot(dat, aes(NDJFM.PDO, value, color=plot.era)) +
@@ -37,8 +39,8 @@ scatter <- ggplot(dat, aes(NDJFM.PDO, value, color=plot.era)) +
   facet_wrap(~key, scales="free_y") + 
   scale_color_manual(values=cb[2:4]) + 
   geom_smooth(method="lm", se=F) + 
-  ylab("Anomaly") + xlab("PDO Index (Nov-Mar)") +
-  theme(legend.title = element_blank())
+  xlab("PDO Index (Nov-Mar)") +
+  theme(legend.title = element_blank(), axis.title.y = element_blank())
 
 ggsave("PDO vs NPI ssh sst stress.png", width = 5, height = 5, units="in")
 
@@ -55,6 +57,24 @@ names(dat)[2] <- "pdo"
 #era as function
 dat$era <- as.factor(dat$era)
 
+# remove units from labels
+dat$key <- as.character(dat$key)
+
+change <- grep("North P", dat$key)
+dat$key[change] <- "North Pacific Index (Nov-Mar)"
+
+change <- grep("Wind", dat$key)
+dat$key[change] <- "Wind stress (Feb-Apr)"
+
+change <- grep("height", dat$key)
+dat$key[change] <- "Sea surface height (Feb-Apr)"
+
+change <- grep("temp", dat$key)
+dat$key[change] <- "Sea surface temp. (Nov-Mar)"
+
+dat$key <- factor(dat$key)
+dat$key <- reorder(dat$key, dat$key.order)
+
 ## fit a model with era-specific intercepts and slopes 
 
 ## none of the slopes on pdo appear to differ among eras - 
@@ -66,17 +86,17 @@ era_NPI_2 <- stan_glm(scale(value) ~ era + pdo,
                     warmup = 1000, iter = 4000, refresh = 0)
 
 era_stress_2 <- stan_glm(scale(value) ~ era + pdo,
-                       data = dat[dat$key == "GOA Wind stress (Feb-Apr)", ],
+                       data = dat[dat$key == "Wind stress (Feb-Apr)", ],
                        chains = 4, cores = 4, thin = 1,
                        warmup = 1000, iter = 4000, refresh = 0)
 
 era_SSH_2 <- stan_glm(scale(value) ~ era + pdo,
-                    data = dat[dat$key == "GOA Sea surface height (Feb-Apr)", ],
+                    data = dat[dat$key == "Sea surface height (Feb-Apr)", ],
                     chains = 4, cores = 4, thin = 1,
                     warmup = 1000, iter = 4000, refresh = 0)
 
 era_SST_2 <- stan_glm(scale(value) ~ era + pdo,
-                    data = dat[dat$key == "GOA Sea surface temp. (Nov-Mar)", ],
+                    data = dat[dat$key == "Sea surface temp. (Nov-Mar)", ],
                     chains = 4, cores = 4, thin = 1,
                     warmup = 1000, iter = 4000, refresh = 0)
 
