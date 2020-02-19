ab55f7802d62cd844c7285ca078198f4c7e8928e
Mike Litzow <mlitzow@alaska.edu>
2020-02-18 17:54:46 -0900

    Added Papa and GAK1 salinity

--- PDO and climate.R
+++ PDO and climate.R
@@ -188,15 +188,6 @@ mdf_indv_arm <- reshape2::melt(coef_indv_arm, id.vars = "key")
 
 
 
-# make a combined plot
-library(ggpubr)
-
-png("era-specific PDO and climate.png", 6, 10, units='in', res=300)
-ggarrange(scatter, int, ncol=1, nrow=3)
-dev.off()
-
-
-
 ## Bayesian model diagnostics
 post_era_NPI_2    <- as.array(era_NPI_2)
 post_era_stress_2 <- as.array(era_stress_2)
@@ -221,3 +212,105 @@ range(summary(era_SSH_2[["stanfit"]])[["summary"]][ , "n_eff"])
 range(summary(era_SSH_2[["stanfit"]])[["summary"]][ , "Rhat"])
 range(summary(era_SST_2[["stanfit"]])[["summary"]][ , "n_eff"])
 range(summary(era_SST_2[["stanfit"]])[["summary"]][ , "Rhat"])
+
+#######################################################
+# now run/plot the negative results
+dat <- read.csv("data/climate.data.csv", row.names = 1)
+
+dat <- dat %>%
+  select(Papa, GAK1.sal, era, NDJFM.PDO)
+
+# now plot relative to PDO!
+colnames(dat)[c(1,2)] <- c("Papa index (ºN, Dec-Feb)",
+                               "GAK1 salinity (psu, Feb-Apr)")
+dat <- dat %>%
+  gather(key, value, -NDJFM.PDO, -era)
+
+dat$plot.era <- ifelse(dat$era==1, "1964-1988",
+                       ifelse(dat$era==2, "1989-2013", "2014-2019"))
+
+
+scatter <- ggplot(dat, aes(NDJFM.PDO, value, color=plot.era)) +
+  theme_bw() +
+  geom_point() +
+  facet_wrap(~key, scales="free_y") +
+  scale_color_manual(values=cb[2:4]) +
+  geom_smooth(method="lm", se=F) +
+  xlab("PDO Index (Nov-Mar)") +
+  theme(legend.title = element_blank(), axis.title.y = element_blank())
+
+# rename pdo
+names(dat)[2] <- "pdo"
+
+#era as function
+dat$era <- as.factor(dat$era)
+
+
+## fit model with era-specific intercepts 
+
+location <- mean(dat$value[dat$key == "Papa index (ºN, Dec-Feb)"])
+
+era_Papa_2 <- stan_glm(value ~ era + pdo,
+                      data = dat[dat$key == "Papa index (ºN, Dec-Feb)", ],
+                      chains = 4, cores = 4, thin = 1,
+                      warmup = 1000, iter = 4000, refresh = 0,
+                      prior = normal(location = 0, scale = 5, autoscale = FALSE),
+                      prior_intercept = normal(location = location, scale = 5, autoscale = FALSE),
+                      prior_aux = student_t(df = 3, location = 0, scale = 5, autoscale = FALSE))
+
+location <- mean(dat$value[dat$key == "GAK1 salinity (psu, Feb-Apr)"])
+
+era_salinity_2 <- stan_glm(value ~ era + pdo,
+                         data = dat[dat$key == "GAK1 salinity (psu, Feb-Apr)", ],
+                         chains = 4, cores = 4, thin = 1,
+                         warmup = 1000, iter = 4000, refresh = 0,
+                         prior = normal(location = 0, scale = 5, autoscale = FALSE),
+                         prior_intercept = normal(location = 0, scale = 5, autoscale = FALSE),
+                         prior_aux = student_t(df = 3, location = 0, scale = 5, autoscale = FALSE))
+
+
+lst <- list(era_Papa_2, era_salinity_2)
+
+lst <- lapply(lst, function(x) {
+  beta <- as.matrix(x, pars = c("(Intercept)", "era2", "era3"))
+  data.frame(key = unique(x$data$key),
+             era1 = beta[ , 1],
+             era2 = beta[ , 1] + beta[ , 2],
+             era3 = beta[ , 1] + beta[ , 3])
+})
+coef_indv_arm <- plyr::rbind.fill(lst)
+mdf_indv_arm <- reshape2::melt(coef_indv_arm, id.vars = "key")
+
+for(i in 1:length(unique(coef_indv_arm$key))) {
+  
+  sub = dplyr::filter(coef_indv_arm, key == unique(coef_indv_arm$key)[i])
+  # calculate pairwise overlaps in slopes and intercepts
+  int_overlap = overlapping::overlap(x = list(int1 = sub$era1,int2=sub$era2,int3=sub$era3))
+  saveRDS(int_overlap$OV,file=paste0(sub$key[1], "_climate_int_overlap.rds"))
+}
+
+
+
+int_tab <- plyr::ddply(mdf_indv_arm, .(key, variable), summarize,
+                       mean = mean(value),
+                       lower95 = quantile(value, probs = 0.025),
+                       upper95 = quantile(value, probs = 0.975))
+
+
+
+int <- ggplot(mdf_indv_arm, aes(x = value, fill = variable)) +
+  theme_bw() +
+  geom_density(alpha = 0.7) +
+  scale_fill_manual(values = c(cb[2], cb[3], cb[4]), labels=c("1964-1988", "1989-2013", "2014-2019")) +
+  theme(legend.title = element_blank()) +
+  labs(x = "Intercept",
+       y = "Posterior density") +
+  facet_wrap( ~ key, scales="free")
+print(int)
+
+# make a combined plot
+library(ggpubr)
+
+png("era-specific PDO and climate.png", 6.5, 7.5, units='in', res=300)
+ggarrange(scatter, int, ncol=1, nrow=2, labels=c("a)", "b)"))
+dev.off()
